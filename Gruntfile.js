1f2d10086ce2dd9f4de522267e0cd44d33d7cbff
Conrad VanLandingham <cv@utexas.edu>
2014-08-07 18:12:19 -0500

    added new deploy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,35 @@ module.exports = function(grunt) {
 
     'gh-pages': {
       options: {
-        base: 'tmp/dist'
+        base: 'dist'
       },
       src: '**/*'
+    },
+
+    'useminPrepare': {
+      html: 'dist/*.html'
+    },
+
+    usemin: {
+      html: 'dist/*.html'
     }
     
   });
 
   grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-filerev');
+
+  // simple build task
+  grunt.registerTask('build', [
+    'useminPrepare:html',
+    'concat:generated',
+    'cssmin:generated',
+    'uglify:generated',
+    'usemin:html'
+  ]);
 
 };
