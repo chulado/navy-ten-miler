1519ef82a2c2622541239e4822e4c336746e7c12
Conrad VanLandingham <cv@utexas.edu>
2014-11-15 20:44:09 -0600

    syncing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -83,7 +83,7 @@ $("a[href^=#]").click(function(e) {
   if (!$e)
     return;
 
-  TweenMax.to(window, 2.5, {scrollTo:{y:$e.offset().top}, ease:Power2.easeInOut});
+  TweenMax.to(window, 2.5, {scrollTo:{y:$e.offset().top - $(".navbar-fixed-top").height()}, ease:Power2.easeInOut});
 });
 
 
@@ -178,28 +178,77 @@ $(document).ready(function() {
   }
 
 
-  // grid
-  // $('.swag-grid').photosetGrid({
-  //   gutter: "2px",
-  //   onComplete: function() {
-  //     $(".swag-grid .photoset-cell img").each(function(){
-  //       $this = $(this);
-  //       var caption = $this.attr("data-caption");
-  //       var div = $("<div>");
+  // clock
 
-  //       div.css({height: Math.round($this.parents(".photoset-row").outerHeight()/2)*2+"px"});
-        
+  var labels = ['weeks', 'days', 'hours', 'minutes', 'seconds'],
+       nextYear = '2015/10/11',
+       currDate = '00:00:00:00:00',
+       nextDate = '00:00:00:00:00',
+       parser = /([0-9]{2})/gi,
+       $example = $(".timer");
 
-  //       div.addClass("caption").html("<p>"+caption+"</p>");
-  //       $this.after(div); //.after("<div class=\"arrowed-up arrowed-dark\">");
-  //     });
-
-  //     $(window).resize(function(){
-  //       $(".swag-grid .photoset-cell .caption").each(function(){
-  //         $(this).css({height: Math.round($(this).parents(".photoset-row").outerHeight()/2)*2+"px"});
-  //       });
-  //     });
-  //   }
-  // });
+       var template = _.template(""+
+       '<div class="time <%= label %>">'+
+         '<span class="count curr top"><%= curr %></span>'+
+         '<span class="count next top"><%= next %></span>'+
+         '<span class="count next bottom"><%= next %></span>'+
+         '<span class="count curr bottom"><%= curr %></span>'+
+         '<span class="label"><%= label.length < 6 ? label : label.substr(0, 3)  %></span>'+
+       '</div>');
+     // Parse countdown string to an object
+     function strfobj(str) {
+       var parsed = str.match(parser),
+         obj = {};
+       labels.forEach(function(label, i) {
+         obj[label] = parsed[i]
+       });
+       return obj;
+     }
+     // Return the time components that diffs
+     function diff(obj1, obj2) {
+       var diff = [];
+       labels.forEach(function(key) {
+         if (obj1[key] !== obj2[key]) {
+           diff.push(key);
+         }
+       });
+       return diff;
+     }
+     // Build the layout
+     var initData = strfobj(currDate);
+     labels.forEach(function(label, i) {
+       $example.append(template({
+         curr: initData[label],
+         next: initData[label],
+         label: label
+       }));
+     });
+     // Starts the countdown
+     $example.countdown(nextYear, function(event) {
+       var newDate = event.strftime('%w:%d:%H:%M:%S'),
+         data;
+       if (newDate !== nextDate) {
+         currDate = nextDate;
+         nextDate = newDate;
+         // Setup the data
+         data = {
+           'curr': strfobj(currDate),
+           'next': strfobj(nextDate)
+         };
+         // Apply the new values to each node that changed
+         diff(data.curr, data.next).forEach(function(label) {
+           var selector = '.%s'.replace(/%s/, label),
+               $node = $example.find(selector);
+           // Update the node
+           $node.removeClass('flip');
+           $node.find('.curr').text(data.curr[label]);
+           $node.find('.next').text(data.next[label]);
+           // Wait for a repaint to then flip
+           _.delay(function($node) {
+             $node.addClass('flip');
+           }, 250, $node);
+         });
+       }
+     });
 
 });
